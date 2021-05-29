b92561e39eb0b1b004887e04faec3019f3c9acd4
Hyunjin Cho <jhj176610@gmail.com>
2021-05-29 13:27:40 +0900

    Add a CSS and function about menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 "use strick";
+
+// Toggle Menu
+const toggleMenu = document.querySelector(".toggleMenu");
+const toggleBtn = document.querySelector(".toggleBtn");
+const menuItems = document.querySelector(".menu-items");
+
+toggleMenu.onmouseover = handleToggleBtnOver;
+toggleMenu.onmouseout = handleToggleBtnOut;
+
+function handleToggleBtnOver() {
+  toggleMenu.style.width = "270px";
+}
+
+function handleToggleBtnOut() {
+  toggleMenu.style.width = "0px";
+}
