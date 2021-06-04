4729bc74187ddffe63a28de8041a9f229cb885cb
Hyunjin Cho <jhj176610@gmail.com>
2021-06-05 03:03:37 +0900

    Add html,css and JS file about tracking sheet

---
+++ src/tracking.js
@@ -0,0 +1,7 @@
+const trackingSheetForm = document.querySelector(".trackingSheetForm");
+const enteredWork = document.querySelector(".inputForTracking");
+const trackingSheetUl = document.querySelector(".trackingSheet-ul");
+
+function init() {}
+
+init();
