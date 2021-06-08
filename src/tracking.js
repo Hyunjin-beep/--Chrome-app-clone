9f2c6a08a59e13250e829d640c2558d95c1bf73f
Hyunjin Cho <jhj176610@gmail.com>
2021-06-09 00:12:17 +0900

    Update related to 24 tracking sheet

--- src/tracking.js
+++ src/tracking.js
@@ -6,17 +6,13 @@ const trackingForms = document.querySelectorAll(".trackingForm");
 const enteredWorks = document.querySelectorAll(".inputForTracking");
 const trackingSheetUl = document.querySelector(".trackingSheet-ul");
 
-function displayTrackList(event, enteredTask) {
-  const input = event.target.firstElementChild;
-  const inputParent = input.parentElement;
+function displayTrackList(enteredTask) {
   const enteredTasksSpan = document.createElement("span");
   enteredTasksSpan.innerText = enteredTask;
 
-  inputParent.appendChild(enteredTasksSpan);
-  inputParent.removeChild(input);
+  form.append(enteredTasksSpan);
 
   const taskObj = {
-    event,
     enteredTask,
     id: tracking_Array.length + 1,
   };
@@ -32,19 +28,28 @@ function saveTrackingList(tracking_Array) {
 function loadTrackList() {
   const loadedTrackList = localStorage.getItem(tracking_ls);
   //   if (loadedTrackList !== null) {
+  //     console.log(loadedTrackList);
   //     const parsedTrackList = JSON.parse(loadedTrackList);
-  //     parsedTrackList.forEach((list) => {
-  //       displayTrackList(list.event, list.enteredTask);
-  //     });
+  //     console.log(parsedTrackList);
+  //     parsedTrackList.forEach(trackList => );
   //   }
 }
 
 function handleTest(event) {
   event.preventDefault();
-  const input = event.target.firstElementChild;
-  const inputParent = input.parentElement;
+  //   console.dir(event);
+  //   console.log(event.target);
+  //   console.log(event.target[0]);
+  const formClassName = event.target.className.substring(12, 15);
+  const inputClassName = event.target[0].className.substring(16, 19);
   const enteredValue = event.target.firstElementChild.value;
-  displayTrackList(input, enteredValue);
+
+  let form = document.getElementsByClassName(`trackingForm ${formClassName}`);
+  let input = document.getElementsByClassName(
+    `inputForTracking ${inputClassName}`
+  );
+
+  displayTrackList(enteredValue);
 }
 
 function init() {
